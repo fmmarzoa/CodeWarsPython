9629b231d591ebfac360cf8ed3c6839d2f3ae142
Fran Marzoa <fmmarzoa@gmail.com>
2017-06-08 00:21:45 +0200

    Recoding everything upside now using numpy

--- conways/ConwaysGameOfLife.py
+++ conways/ConwaysGameOfLife.py
@@ -1,24 +1,10 @@
+import numpy as np
+
+def crop_array(arr):
+    # TODO remove columns and rows with only zeroes on outside margins
+
 def get_generation(cells, generations):
-    next_gen = list(map(list, cells))
-    while generations:
-        prev_gen = list(map(list, next_gen))
-        print(str(generations) + ". " + str(prev_gen))
-        for row in range(0, len(prev_gen)):
-            row_width = len(prev_gen[row])
-            for col in range(0, row_width):
-                nbh_top = row - 1 if row != 0 else 0
-                nbh_left = col - 1 if col != 0 else 0
-                nbh_bottom = row + 1 if row != len(prev_gen) - 1 else len(prev_gen) - 1
-                nbh_right = col + 1 if col != row_width - 1 else row_width - 1
-                alive_neighbors = -1 if prev_gen[row][col] else 0
-                for nbh_row in range(nbh_top, nbh_bottom + 1):
-                    for nbh_col in range(nbh_left, nbh_right + 1):
-                        if prev_gen[nbh_row][nbh_col]:
-                            alive_neighbors += 1
-                if prev_gen[row][col] == 0:
-                    next_gen[row][col] = 1 if (alive_neighbors == 3) else 0
-                else:
-                    next_gen[row][col] = 1 if (4 > alive_neighbors > 1) else 0
-        generations -= 1
-    print(str(generations) + ". " + str(next_gen))
-    return next_gen
+    current_gen = np.array(cells)
+    next_gen = np.zeros((current_gen.shape[0] + 2, current_gen.shape[1] + 2), dtype=np.int)
+    next_gen[1:current_gen.shape[1] + 1, 1:current_gen.shape[1] + 1] = current_gen
+    return cells
