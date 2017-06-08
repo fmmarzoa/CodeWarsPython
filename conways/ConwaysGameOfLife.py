f52f6c0bda3619da5b5f2a78509fe111dfd2c362
Fran Marzoa <fmmarzoa@gmail.com>
2017-06-08 16:06:52 +0200

    Minor changes

--- conways/ConwaysGameOfLife.py
+++ conways/ConwaysGameOfLife.py
@@ -1,48 +1,52 @@
 import numpy as np
 
 
-def get_neighbourhood(arr, row, col):
+def get_neighbourhood(array, row, col):
     """Returns the complete neighbour for the cell 
-       at row, col in the array arr"""
+       at row, col in the array"""
     top = row - 1 if row > 0 else 0
-    bottom = row + 1 if row < arr.shape[0] - 1 else arr.shape[0]
+    bottom = row + 1 if row < array.shape[0] - 1 else array.shape[0]
     left = col - 1 if col > 0 else 0
-    right = col + 1 if col < arr.shape[1] - 1 else arr.shape[1]
-    return arr[top:bottom+1, left:right+1]
+    right = col + 1 if col < array.shape[1] - 1 else array.shape[1]
+    return array[top:bottom + 1, left:right + 1]
 
 
-def crop_array(arr):
+def crop_array(array):
     """Removes columns and rows with only zeroes 
        on outside margins"""
-    while arr.size and np.count_nonzero(arr[0]) == 0:
+    cropped = np.array(array)
+    while cropped.size and np.count_nonzero(cropped[0]) == 0:
         # Remove rows from top to bottom until we find a non-zero one
-        arr = arr[1:]
-    while arr.size and np.count_nonzero(arr[-1]) == 0:
+        cropped = cropped[1:]
+    while cropped.size and np.count_nonzero(cropped[-1]) == 0:
         # Remove rows from bottom to top until we find a non-zero one
-        arr = arr[:-1]
-    while arr.size and np.count_nonzero(arr[0:, 0]) == 0:
+        cropped = cropped[:-1]
+    while cropped.size and np.count_nonzero(cropped[0:, 0]) == 0:
         # Remove rows from left to right until we find a non-zero one
-        arr = arr[0:, 1:]
-    while arr.size and np.count_nonzero(arr[0:, -1]) == 0:
+        cropped = cropped[0:, 1:]
+    while cropped.size and np.count_nonzero(cropped[0:, -1]) == 0:
         # Remove rows from right to left until we find a non-zero one
-        arr = arr[0:, :-1]
-    return arr
+        cropped = cropped[0:, :-1]
+    return cropped
+
+
+def frame_array(array):
+    framed_array = np.zeros((array.shape[0] + 2, array.shape[1] + 2), dtype=np.int)
+    framed_array[1:array.shape[0] + 1, 1:array.shape[1] + 1] = array
+    return framed_array
 
 
 def get_generation(cells, generations):
-    print ("gens:" + str(generations) + "\ncells: " + str(cells))
+    # print("gens:" + str(generations) + "\ncells: " + str(cells))
     current_gen = np.array(cells)
-    # Create a new zero-filled matrix two rows and columns wider
-    next_gen = np.zeros((current_gen.shape[0] + 2, current_gen.shape[1] + 2), dtype=np.int)
-    # Insert the current generation matrix in the middle of the next generation one
-    next_gen[1:current_gen.shape[0] + 1, 1:current_gen.shape[1] + 1] = current_gen
     while generations:
-        current_gen = np.array(next_gen)
-        for row in range(0, current_gen.shape[0]):
-            for col in range(0, current_gen.shape[1]):
-                neighbourhood = get_neighbourhood(current_gen, row, col)
+        framed_array = frame_array(current_gen)
+        next_gen = np.array(framed_array)
+        for row in range(0, framed_array.shape[0]):
+            for col in range(0, framed_array.shape[1]):
+                neighbourhood = get_neighbourhood(framed_array, row, col)
                 alive_neighbours = np.count_nonzero(neighbourhood)
-                if current_gen[row, col]:
+                if framed_array[row, col]:
                     # Cell is alive
                     # Remove the cell we are checking itself from the alive_neighbours count
                     alive_neighbours -= 1
@@ -54,5 +58,6 @@ def get_generation(cells, generations):
                     if alive_neighbours == 3:
                         # Revive cell if there are exactly three neighbours alive
                         next_gen[row, col] = 1
+        current_gen = crop_array(next_gen)
         generations -= 1
-    return crop_array(next_gen).tolist()
+    return current_gen.tolist()
